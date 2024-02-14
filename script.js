563915322383bee18a3ed50eb8a06577610d0b1f
EasChen70 <eason_chen@uri.edu>
2024-02-13 23:00:18 -0500

    Progress on button event listener, changing to DOM soon

--- script.js
+++ script.js
@@ -10,24 +10,18 @@ function getComputerChoice(){
     }
 }
 
-//Get buttons + id name then passes to getPlayerChoice function
-const buttons = document.querySelectorAll('button').forEach(button => {
-    button.addEventListener('click', function() {
-        const choice = this.getAttribute('id');
-        getPlayerChoice(choice);
-    });
-});
 
 
-function getPlayerChoice(input){
-    let choice = input.toLowerCase();
-    if(choice === "earth" || choice === "water" || choice === "fire"){
-        console.log(choice);
-        return choice;
-    }else{
-        console.log("Something went wrong!");
-    }  
-}
+
+// function getPlayerChoice(input){
+//     let choice = input.toLowerCase();
+//     if(choice === "earth" || choice === "water" || choice === "fire"){
+//         console.log(choice);
+//         return choice;
+//     }else{
+//         console.log("Something went wrong!");
+//     }  
+// }
 
 
 function playRound(playerChoice, computerChoice){
@@ -45,35 +39,42 @@ function playRound(playerChoice, computerChoice){
     }
 }
 
-function playGame(){
-    let userScore = 0;
-    let computerScore = 0;
-    for(i = 0; i < 5; i++){
-        let playerChoice = getPlayerChoice();
-        let computerChoice = getComputerChoice();
-        let result = playRound(playerChoice, computerChoice);
 
-        switch (result) {
-            case 0:
-                console.log(`Round ${i + 1}: It's a Tie!`);
-                break;
-            case 1:
-                console.log(`Round ${i + 1}: You Win! ${playerChoice} beats ${computerChoice}`);
-                userScore++;
-                break;
-            case 2:
-                console.log(`Round ${i + 1}: You Lose! ${computerChoice} beats ${playerChoice}`);
-                computerScore++;
-                break;
+let userScore = 5;
+let computerScore = 5;    
+let i = 0;
+//Get buttons + id name then passes to getPlayerChoice function
+const buttons = document.querySelectorAll('button').forEach(button => {
+    button.addEventListener('click', function() {
+        if(userScore > 0 && computerScore > 0){
+            const playerChoice = this.getAttribute('id').toLowerCase();
+            const computerChoice = getComputerChoice();
+            let result = playRound(playerChoice, computerChoice);
+            i++;
+                switch (result) {
+                    case 0:
+                        console.log(`Round ${i + 1}: It's a Tie!`);
+                        break;
+                    case 1:
+                        console.log(`Round ${i + 1}: You Win! ${playerChoice} beats ${computerChoice}`);
+                        computerScore--;
+                        break;
+                    case 2:
+                        console.log(`Round ${i + 1}: You Lose! ${computerChoice} beats ${playerChoice}`);
+                        userScore--;
+                        break;
+                }
+            
+            if(computerScore == 0){
+                console.log("You Won!")
+            } else if (userScore == 0){
+                console.log("You Lost!")
+            }
         }
-    }
-    if(userScore > computerScore){
-        console.log("You Won!")
-    } else if (userScore < computerScore){
-        console.log("You Lost!")
-    } else{
-        console.log("It's A Tie!")
-    }
-}
+    });
+});
 
-playGame();
+        
+
+
+
